49891561825382f0c5ed3c528138459203f368cf
Bárbara Maximo <barbara.cmaximo@gmail.com>
2024-05-21 15:25:32 -0300

    Create graphql project

---
+++ src/graphql/resolvers/mutations/index.ts
@@ -0,0 +1,11 @@
+import { GraphQLResolveInfo } from 'graphql'
+import {  mutationTest } from './mutationTest'
+
+export default {
+  mutationTest: (
+    parent: any,
+    args: any,
+    context: any, 
+    info: GraphQLResolveInfo
+  ) => mutationTest(parent, args, context, info)
+}
---
+++ src/graphql/resolvers/mutations/mutationTest.ts
@@ -0,0 +1,4 @@
+
+export const mutationTest = (_parent, args, _context, _info) => {
+  return args.test
+}
